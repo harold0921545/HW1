27e7bdd62a34b3aa14c383042fb3ef4408d0a511
shaman <62131460+harold0921545@users.noreply.github.com>
2024-08-19 20:52:06 +0800

    Initial commit

---
+++ hw/test/Delegation/BankBase.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Test, console2} from "forge-std/Test.sol";
+
+import {Bank} from "../../src/Delegation/Bank.sol";
+
+contract BankBaseTest is Test {
+    /// State Variable
+
+    // Role
+    address internal user;
+    address internal victim;
+
+    // Contract
+    Bank internal bank;
+
+    /// modifier
+    modifier validation() {
+        assertTrue(address(bank).balance == 100 ether);
+        _;
+        assertTrue(bank.balances(address(this)) == 100 ether);
+    }
+
+    /// Setup function
+    function setUp() public {
+        // Role
+        victim = makeAddr("victim");
+        deal(victim, 100 ether);
+
+        user = makeAddr("user");
+        deal(user, 1 ether);
+
+        // Contract
+        bank = new Bank();
+
+        vm.prank(victim);
+        bank.deposit{value: victim.balance}();
+    }
+}
